08eaa67de40fa225edfeb242ed3f8650ac55f33c
frysee <frysee@googlemail.com>
2025-02-13 15:46:45 +0100

    added minui package

--- packages/virtual/image/package.mk
+++ packages/virtual/image/package.mk
@@ -13,9 +13,10 @@ PKG_DEPENDS_TARGET="toolchain squashfs-tools:host dosfstools:host fakeroot:host 
                     ${BOOTLOADER} busybox umtprd util-linux usb-modeswitch unzip poppler jq socat \
                     p7zip file initramfs grep wget util-linux btrfs-progs zstd lz4 empty lzo libzip \
                     bash coreutils modules system-utils autostart quirks powerstate gnupg \
-                    gzip six lynx xmlstarlet vim pyudev dialog dbus-python network rocknix"
+                    gzip six lynx xmlstarlet vim pyudev dialog dbus-python network rocknix minui"
 
-PKG_UI="emulationstation es-themes textviewer"
+#PKG_UI="emulationstation es-themes textviewer"
+PKG_UI="textviewer"
 
 PKG_UI_TOOLS="fileman fbgrab grim"
 
