d766659fb6cc4df82eeefb29b616effedbc41c08
frysee <frysee@googlemail.com>
2025-02-16 19:49:42 +0100

    Add new precompiled splash from rocknix instead of plymouth-based one

---
+++ packages/tools/mess-splash/package.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2025 ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="mess-splash"
+PKG_VERSION="0.1"
+PKG_LICENSE="GPL"
+PKG_SITE="https://rocknix.org"
+PKG_URL="https://github.com/frysee/${PKG_NAME}/archive/${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_INIT="toolchain"
+PKG_LONGDESC="MESS splash screen application"
